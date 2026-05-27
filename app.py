fe0828ac28ac93ca2a6f542220d0f21c3bf52016
khfaji495-ctrl <khfaji495@gmail.com>
2026-05-27 11:49:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def prepare_arabic_text(text):
 
 # 4. واجهة رفع الملفات
 st.divider()
-uploaded_file = st.file_uploader("📂 اسحب ملف الملزمة هنا (PDF)", type="pdf")
+uploaded_file = st.file_uploader("😸 اسحب ملف الملزمة هنا (PDF)", type="pdf")
 
 if uploaded_file is not None:
     doc = fitz.open(stream=uploaded_file.read(), filetype="pdf")
